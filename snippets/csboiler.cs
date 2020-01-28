36943a6902f21d304e0662c3d17267c9afa54ec2
Simon Renblad <simon.renblad@outlook.com>
2020-01-28 20:39:17 +0800

    added snippets

---
+++ snippets/csboiler.cs
@@ -0,0 +1,21 @@
+// C# .NET
+//
+// DocString
+//
+// author: Simon Renblad
+// version: 0.1.0
+// license: MIT
+
+using System;
+
+namespace Namespace1 {
+
+    class MainClass {
+        
+        public static void Main(string[] args) {
+
+        }
+
+    }
+
+}
---
+++ snippets/pyboiler.py
@@ -0,0 +1,17 @@
+# python3
+"""
+Module Docstring
+"""
+
+__author__ = "Simon Renblad"
+__version__ = "0.1.0"
+__license__ = "MIT"
+
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
